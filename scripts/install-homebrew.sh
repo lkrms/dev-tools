fd5403b06438684f943940d508ef862fe93918b8
Luke Arms <luke@linacreative.com>
2018-02-26 14:38:56 +1100

    Added pandoc to Homebrew script.

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -15,11 +15,20 @@ brew install \
     mariadb \
     micro \
     node \
+    pandoc \
     pv \
     rsync \
     wget \
     youtube-dl \
     || exit 2
+
+brew cask install mactex || exit 2
+
+if [ ! -e /usr/local/bin/pdflatex -a -e /Library/TeX/Root/bin/x86_64-darwin/pdflatex ]; then
+
+    ln -s /Library/TeX/Root/bin/x86_64-darwin/pdflatex /usr/local/bin/pdflatex
+
+fi
 
 # Uncomment if you need to mount ext4 volumes on your Mac:
 #brew cask install osxfuse
