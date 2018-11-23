0c03e0c28e615f468bc28bd37ea98cd1d56c97a1
Luke Arms <luke@linacreative.com>
2018-11-23 22:05:41 +1100

    Bug fix: WP update check running on wrong condition.

--- wordpress/wp-create-snapshot.sh
+++ wordpress/wp-create-snapshot.sh
@@ -4,7 +4,6 @@ SCRIPT_PATH="${BASH_SOURCE[0]}"
 [ -h "$SCRIPT_PATH" ] && SCRIPT_PATH="$(readlink "$SCRIPT_PATH")"
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd -P)" || exit 2
 
-[ -e "$SCRIPT_DIR/common" ] || exit 2
 . "$SCRIPT_DIR/common"
 
 check_not_root
--- wordpress/wp-install-cli.sh
+++ wordpress/wp-install-cli.sh
@@ -4,11 +4,10 @@ SCRIPT_PATH="${BASH_SOURCE[0]}"
 [ -h "$SCRIPT_PATH" ] && SCRIPT_PATH="$(readlink "$SCRIPT_PATH")"
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd -P)" || exit 2
 
-[ -e "$SCRIPT_DIR/common" ] || exit 2
 . "$SCRIPT_DIR/common"
 
 check_not_root
 
 # only attempt to update if WP-CLI is already installed
-check_wp_cli || update_wp_cli
+check_wp_cli && update_wp_cli
 
