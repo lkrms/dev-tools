08d051967747ee846a9d86f4396ae19eb6b2b179
Luke Arms <luke@linacreative.com>
2018-11-23 22:17:52 +1100

    wp-create-snapshot.sh should be functional now.

--- wordpress/wp-create-snapshot.sh
+++ wordpress/wp-create-snapshot.sh
@@ -18,6 +18,16 @@ SNAPSHOT_TIMESTAMP="$(date +'%Y%m%d-%H%M%S')"
 FILE_ARCHIVE_PATH="$SNAPSHOT_PATH/$SITE_DOMAIN-$SNAPSHOT_TIMESTAMP.tar.gz"
 SQL_ARCHIVE_PATH="$SNAPSHOT_PATH/$SITE_DOMAIN-$SNAPSHOT_TIMESTAMP.sql.gz"
 
-echo tar zcf "$FILE_ARCHIVE_PATH" .
-echo $MYSQLDUMP_COMMAND '|' gzip '>' "$SQL_ARCHIVE_PATH"
+mkdir -p "$SNAPSHOT_PATH"
+
+echo -e "Dumping files and database for $SITE_URL to $SNAPSHOT_PATH..."
+
+tar zcf "$FILE_ARCHIVE_PATH" .
+TAR_RESULT="$?"
+
+$MYSQLDUMP_COMMAND $DB_NAME | gzip > "$SQL_ARCHIVE_PATH"
+MYSQLDUMP_RESULT="${PIPESTATUS[0]}"
+
+echo -e "Files dumped to: $FILE_ARCHIVE_PATH (tar exit code: $TAR_RESULT)"
+echo -e "Database dumped to: $SQL_ARCHIVE_PATH (mysqldump exit code: $MYSQLDUMP_RESULT)"
 
