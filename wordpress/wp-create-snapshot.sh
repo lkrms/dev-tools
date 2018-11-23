c2119e6825b3e318439d04ab04ce4d6b8eacce83
Luke Arms <luke@linacreative.com>
2018-11-23 21:59:11 +1100

    Bug fix: sourcing common was returning non-zero

--- wordpress/wp-create-snapshot.sh
+++ wordpress/wp-create-snapshot.sh
@@ -4,7 +4,8 @@ SCRIPT_PATH="${BASH_SOURCE[0]}"
 [ -h "$SCRIPT_PATH" ] && SCRIPT_PATH="$(readlink "$SCRIPT_PATH")"
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd -P)" || exit 2
 
-. "$SCRIPT_DIR/common" || exit 2
+[ -e "$SCRIPT_DIR/common" ] || exit 2
+. "$SCRIPT_DIR/common"
 
 check_not_root
 check_wp_cli
