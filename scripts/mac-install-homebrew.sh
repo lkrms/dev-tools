73614003f1ec6a7024d6ac5cfd6d75c37a4cabbf
Luke Arms <luke@linacreative.com>
2019-05-27 15:44:50 +1000

    Add additional packages (including OCR) to mac-install-homebrew.sh

--- scripts/mac-install-homebrew.sh
+++ scripts/mac-install-homebrew.sh
@@ -7,23 +7,46 @@ if [ "$(uname -s)" != "Darwin" ]; then
 
 fi
 
-command -v brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || exit 2
+if command -v brew >/dev/null 2>&1; then
 
-brew upgrade
-brew cask upgrade
+    brew upgrade
+    brew cask upgrade
+
+else
+
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || exit 2
+
+fi
 
 brew install \
     coreutils \
     exiftool \
     ghostscript \
+    gnupg \
     lftp \
     pandoc \
+    poppler \
     pv \
     rsync \
     telnet \
     wget \
     youtube-dl \
     || exit 2
+
+read -p "Install OCR tools? [y/n] " -n 1 -r INSTALL_OCR_TOOLS
+
+echo
+
+if [[ $INSTALL_OCR_TOOLS =~ ^[Yy]$ ]]; then
+
+    brew install \
+        ocrmypdf \
+        tesseract \
+        tesseract-lang \
+        || exit 2
+
+
+fi
 
 read -p "Install developer tools? [y/n] " -n 1 -r INSTALL_DEV_TOOLS
 
@@ -34,11 +57,14 @@ if [[ $INSTALL_DEV_TOOLS =~ ^[Yy]$ ]]; then
     brew install \
         ant \
         autoconf \
+        cmake \
         composer \
         gradle \
         mariadb \
         msmtp \
         nvm \
+        php@7.1 \
+        pkg-config \
         yarn \
         || exit 2
 
