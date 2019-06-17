ef9557ea1b8bdd2271ff484e14d45f225c7df0a9
Luke Arms <luke@linacreative.com>
2019-06-17 19:17:54 +1000

    Add img2pdf to Homebrew installer and fix Yarn/Node.js/nvm conflict

--- scripts/mac-install-homebrew.sh
+++ scripts/mac-install-homebrew.sh
@@ -18,22 +18,33 @@ else
 
 fi
 
+# ghostscript: PDF/PostScript processor
+# imagemagick: image processor
+# pandoc: text conversion tool (e.g. Markdown to PDF)
+# poppler: PDF tools like pdfimages
+# python3: so that we can use pip3 below
 brew install \
     coreutils \
     exiftool \
     ghostscript \
-    gnupg \
+    imagemagick \
     lftp \
     pandoc \
     poppler \
     pv \
+    python3 \
     rsync \
     telnet \
     wget \
     youtube-dl \
     || exit 2
 
-read -p "Install OCR tools? [y/n] " -n 1 -r INSTALL_OCR_TOOLS
+# img2pdf: lossless creation of PDFs from source JPEGs
+pip3 install -U \
+    img2pdf \
+    || exit 2
+
+read -p "Install OCR tools (including tesseract-lang)? [y/n] " -n 1 -r INSTALL_OCR_TOOLS
 
 echo
 
@@ -45,12 +56,13 @@ if [[ $INSTALL_OCR_TOOLS =~ ^[Yy]$ ]]; then
         tesseract-lang \
         || exit 2
 
-
 fi
 
 read -p "Install developer tools? [y/n] " -n 1 -r INSTALL_DEV_TOOLS
 
 echo
+
+OLD_NVM_DIR="$NVM_DIR"
 
 if [[ $INSTALL_DEV_TOOLS =~ ^[Yy]$ ]]; then
 
@@ -62,18 +74,46 @@ if [[ $INSTALL_DEV_TOOLS =~ ^[Yy]$ ]]; then
         gradle \
         mariadb \
         msmtp \
-        nvm \
-        php@7.1 \
+        php@7.2 \
         pkg-config \
-        yarn \
         || exit 2
 
-    mkdir -p "$HOME/.nvm" || exit 2
-    export NVM_DIR="$HOME/.nvm"
-    . "$(brew --prefix nvm)/nvm.sh" || exit 2
+    # don't disturb an existing nvm installation
+    if [ -z "$OLD_NVM_DIR" -o "$OLD_NVM_DIR" = "$HOME/.nvm" ]; then
+
+        export NVM_DIR="$HOME/.nvm"
+
+        if [ ! -e "$NVM_DIR" ]; then
+
+            git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR" || exit 2
+            pushd "$NVM_DIR" >/dev/null || exit 2
+            git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)) || exit 2
+            popd >/dev/null
+
+            . "$NVM_DIR/nvm.sh" || exit 2
+
+            NVM_INSTALLED=1
+
+        elif [ -d "$NVM_DIR/.git" ]; then
+
+            pushd "$NVM_DIR" >/dev/null || exit 2
+            git fetch --tags origin
+            git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)) || exit 2
+            popd >/dev/null
+
+            . "$NVM_DIR/nvm.sh" || exit 2
+
+        fi
+
+    fi
+
     nvm install 8 || exit 2
 
-    npm install -g eslint
+    # work around Homebrew's reluctance to recognise nvm-based node by installing Yarn like this...
+    npm install -g \
+        eslint \
+        yarn \
+        || exit 2
 
 fi
 
@@ -96,6 +136,13 @@ if [[ $INSTALL_TEX =~ ^[Yy]$ ]]; then
     brew cask install basictex || exit 2
     sudo tlmgr update --self && sudo tlmgr install collection-fontsrecommended || exit 2
     luaotfload-tool --update || exit 2
+
+fi
+
+if [ -z "$OLD_NVM_DIR" -a -n "$NVM_DIR" ]; then
+
+    echo -e "\n\nIMPORTANT: nvm is installed, so you need something like this in your "'~/.profile'":\n"
+    echo '[ -e "$HOME/.nvm" ] && export NVM_DIR="$HOME/.nvm" && [ -e "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"'
 
 fi
 
